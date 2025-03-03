89f0b6e866e69f88249e607ccbd723a588937c84
Yassir Aykhlf <aykhlf15yassir@gmail.com>
2025-03-03 17:57:57 +0000

    Add tokenizer

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: yaykhlf <yaykhlf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 13:05:43 by yaykhlf           #+#    #+#             */
-/*   Updated: 2025/03/01 21:11:08 by yaykhlf          ###   ########.fr       */
+/*   Updated: 2025/03/03 17:57:09 by yaykhlf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,67 @@
 # include <errno.h>
 # include <stdbool.h>
 
-typedef struct ASTnode
-{
-	int				type;
-	union{
-		{
-			char		*cmd;
-			char		**args;
-		} binary_node,
-		{
-			[] childs
-			[] redirection;
-		} unary_node;
-		
-	}
+bool	is_whitespace(char c);
 
-}				t_ast;
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIRECT_IN,
+	TOKEN_REDIRECT_OUT,
+	TOKEN_APPEND,
+	TOKEN_HEREDOC,
+	TOKEN_AND,
+	TOKEN_OR,
+	TOKEN_SINGLE_QUOTED,
+	TOKEN_DOUBLE_QUOTED,
+}	t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+}	t_token;
+
+typedef enum e_node_type
+{
+	SIMPLE_COMMAND,    /* Basic command with arguments */
+	PIPELINE,          /* Pipe between commands */
+	AND_NODE,          /* Logical AND (&&) */
+	OR_NODE,           /* Logical OR (||) */
+	COMPOUND_COMMAND,  /* Compound command (a group) */
+	REDIRECT_IN,       /* Input redirection (<) */
+	REDIRECT_OUT,      /* Output redirection (>) */
+	REDIRECT_APPEND,   /* Output append redirection (>>) */
+	HERE_DOC           /* Heredoc (<<) */
+}	t_node_type;
+
+typedef struct s_redir
+{
+	int		type;           /* Type of redirection */
+	char	*file;         /* File name */
+}	t_redir;
+
+typedef struct s_ast
+{
+	t_node_type type;
+	union
+	{
+		/* For command nodes */
+		struct
+		{
+			char *cmd;
+			char **args;
+		} binary_node;
+		
+		/* For nodes with multiple children */
+		struct
+		{
+			t_ast **childs;
+			t_redir **redirection;
+		} unary_node;
+	};
+}	t_ast;
 
 #endif
---
+++ tokenize.c
@@ -0,0 +1,159 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenize.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaykhlf <yaykhlf@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 16:55:37 by yaykhlf           #+#    #+#             */
+/*   Updated: 2025/03/03 17:23:24 by yaykhlf          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/minishell.h"
+
+static t_token	*create_token(t_token_type type, char *value)
+{
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->type = type;
+	token->value = value;
+	token->next = NULL;
+	return (token);
+}
+
+static void	add_token(t_token **head, t_token *new_token)
+{
+	t_token	*current;
+
+	if (!head || !new_token)
+		return ;
+	if (!*head)
+		*head = new_token;
+	else
+	{
+		current = *head;
+		while (current->next)
+			current = current->next;
+		current->next = new_token;
+	}
+}
+
+static char	*extract_quoted_string(const char *input, int *pos, char quote)
+{
+	int		start;
+	int		end;
+	char	*str;
+
+	start = *pos + 1;
+	end = start;
+	while (input[end] && input[end] != quote)
+		end++;
+	str = strndup(input + start, end - start);
+	if (input[end] == quote)
+		*pos = end + 1;
+	else
+		*pos = end;
+	return (str);
+}
+
+static void	handle_operator(const char *input, int *pos, t_token **tokens)
+{
+	if (strncmp(input + *pos, "<<", 2) == 0)
+	{
+		add_token(tokens, create_token(TOKEN_HEREDOC, strdup("<<")));
+		*pos += 2;
+	}
+	else if (strncmp(input + *pos, ">>", 2) == 0)
+	{
+		add_token(tokens, create_token(TOKEN_APPEND, strdup(">>")));
+		*pos += 2;
+	}
+	else if (strncmp(input + *pos, "&&", 2) == 0)
+	{
+		add_token(tokens, create_token(TOKEN_AND, strdup("&&")));
+		*pos += 2;
+	}
+	else if (strncmp(input + *pos, "||", 2) == 0)
+	{
+		add_token(tokens, create_token(TOKEN_OR, strdup("||")));
+		*pos += 2;
+	}
+	else if (input[*pos] == '<')
+	{
+		add_token(tokens, create_token(TOKEN_REDIRECT_IN, strdup("<")));
+		(*pos)++;
+	}
+	else if (input[*pos] == '>')
+	{
+		add_token(tokens, create_token(TOKEN_REDIRECT_OUT, strdup(">")));
+		(*pos)++;
+	}
+	else if (input[*pos] == '|')
+	{
+		add_token(tokens, create_token(TOKEN_PIPE, strdup("|")));
+		(*pos)++;
+	}
+}
+
+t_token	*ft_tokenize(const char *input)
+{
+	t_token	*tokens;
+	int		pos;
+	int		len;
+	int		start;
+	char	*value;
+
+	tokens = NULL;
+	pos = 0;
+	len = strlen(input);
+	while (pos < len)
+	{
+		while (pos < len && is_whitespace(input[pos]))
+			pos++;
+		if (pos >= len)
+			break ;
+		if (input[pos] == '\'')
+		{
+			value = extract_quoted_string(input, &pos, '\'');
+			add_token(&tokens, create_token(TOKEN_SINGLE_QUOTED, value));
+		}
+		else if (input[pos] == '"')
+		{
+			value = extract_quoted_string(input, &pos, '"');
+			add_token(&tokens, create_token(TOKEN_DOUBLE_QUOTED, value));
+		}
+		else if (strchr("<>|&", input[pos]))
+		{
+			handle_operator(input, &pos, &tokens);
+		}
+		else
+		{
+			start = pos;
+			while (pos < len && !is_whitespace(input[pos])
+				&& !strchr("<>|&'\"", input[pos]))
+				pos++;
+			value = strndup(input + start, pos - start);
+			add_token(&tokens, create_token(TOKEN_WORD, value));
+		}
+	}
+	return (tokens);
+}
+
+void	free_tokens(t_token *tokens)
+{
+	t_token	*current;
+	t_token	*next;
+
+	current = tokens;
+	while (current)
+	{
+		next = current->next;
+		free(current->value);
+		free(current);
+		current = next;
+	}
+}
